cd66ed584e2237f6e6d6648911cb19062d684eba
Ajitesh Bhan <60276412+ajitesh-bhan@users.noreply.github.com>
2020-11-04 03:44:23 +0100

    Add files via upload

---
+++ Testing and Traning Database.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sqlite3
+import pandas as pd
+timeframes= ['2015-01']
+# multiple db timeframes can be put
+
+
+def file_creation(filename, data_frame):
+    
+    with open(filename,'a', encoding='utf8') as f:
+            data_frame.to_csv(f,header=False)
+            
+    return 
+
+for t in timeframes:
+    connection = sqlite3.connect('{}.db'.format(t))
+    c=connection.cursor()
+    limit=5000
+    last_unix=0
+    stop_time=limit
+    counter =0
+    test_done=False
+    while stop_time==limit:
+        df = pd.read_sql("SELECT * FROM parent_reply WHERE unix > {} and parent NOT NULL and score > 0 ORDER BY unix ASC LIMIT {}".format(last_unix,limit),connection)
+        last_unix = df.tail(1)['unix'].values[0]
+        # updating last_unix
+        #when to stop
+        stop_time = len(df)
+        data= pd.DataFrame(df, columns = ['parent', 'comment'])
+        if test_done== False: 
+            file_creation('Testing.csv', data)
+            test_done= True
+        else: 
+            file_creation('Training.csv', data)
+        # here my training and test size are nearly equal for 70/30 ratio a new counter could be set to change test_done flag
+        counter +=1
+        if counter % 20 == 0: 
+            print(counter*limit,'rows completed so far')
+            #updates after 1000 rows 
+    
+        
+
